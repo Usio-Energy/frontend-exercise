48645f5b271c4399df71520f74e131dc7e9b98ca
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2018-09-06 10:32:31 +0100

    Rename entry file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js'
